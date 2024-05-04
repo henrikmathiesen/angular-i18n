10f859ddd5d44487a59d2c1f581c478733c8473b
henrikmathiesen <henrik_mathiesen@hotmail.com>
2024-05-04 16:07:42 +0200

    Labbing with signals, the basics

--- src/app/routable-components/signal-inputs/signal-inputs-child/signal-inputs-child.component.ts
+++ src/app/routable-components/signal-inputs/signal-inputs-child/signal-inputs-child.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, Input, EventEmitter, SimpleChanges, OnChanges } from '@angular/core';
+import { Component, Output, Input, EventEmitter, SimpleChanges, OnChanges, input, output, effect } from '@angular/core'; // computed (TODO: Learn more about computed)
 
 @Component({
     selector: 'app-signal-inputs-child',
@@ -7,13 +7,28 @@ import { Component, Output, Input, EventEmitter, SimpleChanges, OnChanges } from
 })
 export class SignalInputsChildComponent implements OnChanges {
 
+    //
+    // Repitition
+
     @Output() childButtonClicked = new EventEmitter(false);
     @Input() parentButtonClicked = false;
-
 
     onChildButtonClick() {
         console.log('SignalInputsChildComponent, onChildButtonClick');
         this.childButtonClicked.emit(true); // communication up to parent via EventEmitter
+
+        //
+        // Signal, communicate up to parent via emit on the OutputEmitter
+
+        this.childButtonClickedSignal.emit(true);
+
+        // Can handle it in parent component in the same way as old eventEmitter
+        // Parent Component
+        // <app-signal-inputs-child (childButtonClicked)="onChildButtonClick()" (childButtonClickedSignal)="onChildButtonClick()" .....>
+
+        //
+        // Can also subscribe in an effect function in Parent Component, read more
+        // https://medium.com/@davidepassafaro/angular-new-output-function-d12e6c31ce2e
     }
 
     ngOnChanges(changes: SimpleChanges): void {
@@ -21,4 +36,21 @@ export class SignalInputsChildComponent implements OnChanges {
         // ngOnChanges will only run if value is changed
         console.log('SignalInputsChildComponent, ngOnChanges', changes['parentButtonClicked'].currentValue);
     }
+
+    //
+    // Signal Inputs
+
+    // 2) Signal, recieved here (hovering over input, it returns an InputSignal). And we use effect in constructor, instead of ngOnChanges
+    parentButtonClickedSignal = input(false);
+    constructor() {
+        effect(() => {
+            // effect will only run if value is changed
+            console.log('SignalInputsChildComponent, effect', this.parentButtonClickedSignal());
+        });
+    }
+
+    // Signal, communicate up to parent
+    childButtonClickedSignal = output<boolean>();
+
+
 }
--- src/app/routable-components/signal-inputs/signal-inputs.component.ts
+++ src/app/routable-components/signal-inputs/signal-inputs.component.ts
@@ -10,6 +10,10 @@ import { SignalInputsChildComponent } from './signal-inputs-child/signal-inputs-
 })
 export class SignalInputsComponent implements RoutableComponentModel {
     componentName = 'SignalInputsComponent';
+
+    //
+    // Repitition
+
     isParentButtonClicked = false;
 
     onParentButtonClick() {
@@ -21,5 +25,17 @@ export class SignalInputsComponent implements RoutableComponentModel {
         console.log('SignalInputsComponent, onChildButtonClick');
     }
 
-    // TODO: https://www.youtube.com/@deborah_kurata/videos
+    /* 
+    
+       Signal Inputs 
+
+        https://www.youtube.com/@deborah_kurata/videos
+        https://www.youtube.com/watch?v=yjCeaiWXC0U&ab_channel=DeborahKurata
+
+        https://blog.angular-university.io/angular-signal-inputs/
+        https://medium.com/@davidepassafaro/angular-new-output-function-d12e6c31ce2e
+
+        TODO: Learn more about computed (see import in Child Component)
+    
+    */
 }
