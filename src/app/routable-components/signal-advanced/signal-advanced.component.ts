2c20d3ade46874a3bad1b0317dc81b78afb64e9f
henrikmathiesen <henrik_mathiesen@hotmail.com>
2024-05-10 16:40:39 +0200

    computed signals

--- src/app/routable-components/signal-advanced/signal-advanced.component.ts
+++ src/app/routable-components/signal-advanced/signal-advanced.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, Signal, signal, computed } from '@angular/core';
 import { RoutableComponentModel } from '../../models';
 
 @Component({
@@ -8,6 +8,24 @@ import { RoutableComponentModel } from '../../models';
 })
 export class SignalAdvancedComponent implements RoutableComponentModel { 
     componentName = 'SignalAdvancedComponent';
+
+    // https://angular.io/guide/signals
+    // https://angular.io/guide/signals#computed-signals
+    // https://www.freecodecamp.org/news/angular-signals/
+
+    count: WritableSignal<number> = signal(0);
+    doubleCount: Signal<number> = computed(() => { return this.count() * 2 });
+
+    onIncreaseCount() {
+        // can use this.count.set()
+        // or increase previous value
+        this.count.update(v => v + 1);
+
+        // Can also mutate properties in reference types
+        // this.selectedVehicle.mutate(v => v.price = v.price + (v.price * .20));
+
+        
+    }   
 
 
 }
